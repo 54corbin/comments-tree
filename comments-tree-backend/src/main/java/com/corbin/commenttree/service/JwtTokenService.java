ba09f6a79cac476675cd3eb76d505a3e480111b9
54corbin <54corbinchen@gmail.com>
2021-09-30 13:08:58 +0800

    docs: 添加了JwtTokenService接口注释文档

--- comments-tree-backend/src/main/java/com/corbin/commenttree/service/JwtTokenService.java
+++ comments-tree-backend/src/main/java/com/corbin/commenttree/service/JwtTokenService.java
@@ -8,12 +8,34 @@ import com.nimbusds.jose.JOSEException;
 import java.text.ParseException;
 
 /**
+ * 生成和校验jwt token
  * @author corbin
  */
 public interface JwtTokenService {
+    /**
+     * 生成jwt token
+     * @param payloadStr token 里携带的内容
+     * @param secret 生成jwt token 所需的的密钥
+     * @return token
+     * @throws JOSEException 无法加密
+     */
     String generateTokenByHMAC(String payloadStr, String secret) throws JOSEException;
 
+    /**
+     * 校验jwt token
+     * @param token token 字符串
+     * @param secret 生成jwt token 所需的的密钥
+     * @return token中携带的数据
+     * @throws ParseException 无法解析token
+     * @throws JOSEException 无法解密
+     * @throws JwtInvalidException 非法token
+     * @throws JwtExpiredException token已过期
+     */
     PayloadDto verifyTokenByHMAC(String token, String secret) throws ParseException, JOSEException, JwtInvalidException, JwtExpiredException;
 
+    /**
+     * 快速生成默认jwt载荷
+     * @return 默认jwt载荷
+     */
     PayloadDto getDefaultPayloadDto();
 }
